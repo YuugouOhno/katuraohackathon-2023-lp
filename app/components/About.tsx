06b382d7ea557073076767d8d8df4952f2eebc15
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 06:41:35 +0900

    added about page

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,96 +1,128 @@
 export default function About() {
     return (
         <div className="bg-katurao-creem w-full px-[15%]">
-            <p className="text-katurao-green">About</p>
-            <div className="text-katurao-black">
-                <p>テーマ</p>
-                <p>「葛尾村の消費活動をもっとワクワクするものへ。」</p>
-            </div>
-            <div className="text-katurao-black">
-                <p>期待すること</p>
-                <p>・消費活動を通じ人のつながりを醸成</p>
-                <p>・新しい生活様式の提案 など</p>
-            </div>
-            <div className="text-katurao-black">
-                <p>期間</p>
-                <p>2023年9月4日-9月16日</p>
-            </div>
-            <div>
-                <div className="bg-katurao-red text-white">
-                    <p>9月4日</p>
-                    <p>キックオフ</p>
+            <p className="text-katurao-green text-title text-7xl font-bold my-24">About</p>
+            <div className="px-[5%] text-xl">
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">テーマ</p>
+                    <p className="font-body">「葛尾村の消費活動をもっとワクワクするものへ。」</p>
                 </div>
-                <div className="text-katurao-black">
-                    <p>Step01</p>
-                    <p>規格と技術の両立の面白さを学ぶ</p>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">期待すること</p>
+                    <p className="mb-3">・消費活動を通じ人のつながりを醸成</p>
+                    <p>・新しい生活様式の提案 など</p>
                 </div>
-                <div className="bg-katurao-brown text-white">
-                    <p>9月5日-14日</p>
-                    <p>オンライン講座</p>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">期間</p>
+                    <p>2023年9月4日-9月16日</p>
                 </div>
-                <div className="text-katurao-black">
-                    <p>Step02</p>
-                    <p>規格と技術の両立の方法を知る</p>
-                </div>
-                <div className="bg-katurao-lightgreen text-white">
-                    <p>9月13日-15日</p>
-                    <p>開発合宿</p>
-                </div>
-                <div className="text-katurao-black">
-                    <p>Step03</p>
-                    <p>規格を形にしてみる</p>
-                </div>
-                <div className="bg-katurao-green text-white">
-                    <p>9月16日</p>
-                    <p>最終発表会</p>
-                </div>
-                <div className="text-katurao-black">
-                    <p>Step04</p>
-                    <p>共有する（仮）</p>
-                </div>
-            </div>
-            <div className="text-katurao-black">
-                <p>会場</p>
-                <p>葛尾村宿泊施設せせらぎ荘</p>
-                <div>写真とか地図とか</div>
-            </div>
-            <div className="text-katurao-black">
-                <p>定員</p>
-                <p>15名（5名×3チーム）</p>
-                <p>※応募者多数の場合は選考の可能性があります。</p>
-            </div>
-            <div className="text-katurao-black">
-                <p>参加対象</p>
-                <p>高専生(4年生以上)・大学生・大学院生。</p>
-                <p>・原則として1チーム５名の参加となりますが、5名に満たないチームでも応募可能です。</p>
-                <p>・複数の高専、大学の学生から構成されるチームでも応募できます。</p>
-            </div>
-            <div className="text-katurao-black">
-                <p>参加費</p>
-                <p>なし。</p>
-                <p>※交通費・食費は参加者負担となります。</p>
-                <p>宿泊費は実行委員会からの全額負担となります。</p>
-            </div>
-            <div className="text-katurao-black">
-                <p>申込締切</p>
-                <p>7月28日まで。</p>
-                <p>選考結果は8月4日頃代表者へご連絡します。</p>
-            </div>
-            <div className="text-katurao-black">
-                <p>表彰</p>
-                <div>WED賞</div>
-                <div>スカラ賞</div>
-            </div>
-            <div className="text-katurao-black">
-                <p>協賛</p>
-                <div>ロゴやらなんやら</div>
-                <div>ロゴやらなんやら</div>
-                <div>ロゴやらなんやら</div>
-            </div>
-            <div className="bg-katurao-green">
-                <p className="text-white">ご応募はこちら</p>
-            </div>
+                <div className="mb-20">
+                    <div className="flex h-40 flex items-center">
+                        <div className="bg-katurao-red text-white font-bold font-title rounded-full w-40 h-40 text-center flex items-center justify-center">
+                            <div>
+                                <p className="mb-3">9月4日</p>
+                                <p>キックオフ</p>
+                            </div>
+                        </div>
+                        <div className="text-katurao-black ml-16">
+                            <p className="font-bold font-title mb-3">Step01</p>
+                            <p>規格と技術の両立の面白さを学ぶ</p>
+                        </div>
+                    </div>
 
+                    <div className="h-12 w-[80px] border-r-4 border-katurao-black"></div>
+
+                    <div className="flex h-40 flex items-center">
+                        <div className="bg-katurao-brown text-white font-bold font-title rounded-full w-40 h-40 text-center flex items-center justify-center">
+                            <div>
+                                <p className="mb-3">9月5日-14日</p>
+                                <p>オンライン講座</p>
+                            </div>
+                        </div>
+                        <div className="text-katurao-black ml-16">
+                            <p className="font-bold font-title mb-3">Step02</p>
+                            <p>規格と技術の両立の方法を知る</p>
+                        </div>
+                    </div>
+
+                    <div className="h-12 w-[80px] border-r-4 border-katurao-black"></div>
+
+                    <div className="flex h-40 flex items-center">
+                        <div className="bg-katurao-lightgreen text-white font-bold font-title rounded-full w-40 h-40 text-center flex items-center justify-center">
+                            <div>
+                                <p className="mb-3">9月13日-15日</p>
+                                <p>開発合宿</p>
+                            </div>
+                        </div>
+                        <div className="text-katurao-black ml-16">
+                            <p className="font-bold font-title mb-3">Step03</p>
+                            <p>規格を形にしてみる</p>
+                        </div>
+                    </div>
+
+                    <div className="h-12 w-[80px] border-r-4 border-katurao-black"></div>
+
+
+                    <div className="flex h-40 flex items-center">
+                        <div className="bg-katurao-green text-white font-bold font-title rounded-full w-40 h-40 text-center flex items-center justify-center">
+                            <div>
+                                <p className="mb-3">9月16日</p>
+                                <p>最終発表会</p>
+                            </div>
+                        </div>
+                        <div className="text-katurao-black ml-16">
+                            <p className="font-bold font-title mb-3">Step04</p>
+                            <p>共有する（仮）</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">会場</p>
+                    <p>葛尾村宿泊施設せせらぎ荘</p>
+                    <div>写真とか地図とか</div>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">定員</p>
+                    <p>15名（5名×3チーム）</p>
+                    <p>※応募者多数の場合は選考の可能性があります。</p>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">参加対象</p>
+                    <p>高専生(4年生以上)・大学生・大学院生。</p>
+                    <p>・原則として1チーム５名の参加となりますが、5名に満たないチームでも応募可能です。</p>
+                    <p>・複数の高専、大学の学生から構成されるチームでも応募できます。</p>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">参加費</p>
+                    <p>なし。</p>
+                    <p>※交通費・食費は参加者負担となります。</p>
+                    <p>宿泊費は実行委員会からの全額負担となります。</p>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">申込締切</p>
+                    <p>7月28日まで。</p>
+                    <p>選考結果は8月4日頃代表者へご連絡します。</p>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">表彰</p>
+                    <div>WED賞</div>
+                    <div>スカラ賞</div>
+                </div>
+                <div className="text-katurao-black mb-20">
+                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">協賛</p>
+                    <div>ロゴやらなんやら</div>
+                    <div>ロゴやらなんやら</div>
+                    <div>ロゴやらなんやら</div>
+                </div>
+                <div className="flex justify-center mb-20">
+                    <div className="bg-katurao-green text-center inline-block rounded-xl py-2 px-8">
+                        <p className="text-white font-bold font-title text-2xl">
+                            ご応募はこちら
+                        </p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
