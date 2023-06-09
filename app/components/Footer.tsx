943e03e3ecf3cf03d4daf61fe896c19ff0475484
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 06:56:59 +0900

    added footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <div className="bg-katurao-green w-full px-[15%]">
-            <div className="text-white">
+        <div className="bg-katurao-green w-full px-[15%] py-32">
+            <div className="text-white font-title text-2xl font-bold">
                 <p>かつらおハッカソン実行委員会</p>
                 <p>katuraohackathon2023@gmail.com</p>
             </div>
