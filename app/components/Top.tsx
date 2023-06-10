94b1cb3a840010c34fc4a6cc52e676ec6ca42d32
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 10:05:09 +0900

    mobile added top page

--- app/components/Top.tsx
+++ app/components/Top.tsx
@@ -1,11 +1,11 @@
 export default function Top() {
     return (
-        <div className="bg-katurao-creem w-full px-[10%] md:px-[15%] pt-32 pb-48 font-title font-black">
-            <p className="text-katurao-green text-8xl">
+        <div className="bg-katurao-creem w-full px-[10%] md:px-[15%] pt-32 pb-40 md:pb-48 font-title font-black">
+            <p className="text-katurao-green text-5xl md:text-8xl">
                 かつらお<br/>
                 ハッカソン
                 </p>
-            <p className="text-katurao-black text-8xl">2023</p>
+            <p className="text-katurao-black text-5xl md:text-8xl">2023</p>
         </div>
     )
 }
