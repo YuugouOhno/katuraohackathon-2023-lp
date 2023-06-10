215d8901730115bf27f7e9f7bc2582a96e411735
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 10:13:13 +0900

    mobile added message page

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -1,17 +1,17 @@
 export default function Message() {
     return (
-        <div className="bg-katurao-yellow  w-full px-[10%] md:px-[15%] text-center items-center">
-            <p className="text-white text-9xl font-title font-black my-28">Message</p>
-            <div className="font-title font-black text-3xl">
-                <p className="text-katurao-black mb-3">葛尾村の消費活動を</p>
+        <div className="bg-katurao-yellow  w-full px-[10%] md:px-[15%] md:text-center items-center">
+            <p className="text-white text-5xl md:text-9xl font-title font-black mt-14 mb-10 md:my-28">Message</p>
+            <div className="font-title font-black text-2xl md:text-3xl">
+                <p className="text-katurao-black mb-1 md:mb-3">葛尾村の消費活動を</p>
                 <p className="text-katurao-black mb-12">もっとワクワクするものへ。</p>
             </div>
-            <div className="text-katurao-black font-body font-light text-2xl mb-28">
-                <p className="mb-3">葛尾村にはコンビニが中央部に一軒しかない。<br/>
-                買い物には損害まで車を走らせる必要がある。</p>
-                <p className="mb-3">一方、村内では体験型牧場など、新たなサービスも生まれつつある。</p>
-                <p className="mb-3">日々の買い物・新たな体験などの消費活動がもっと活発になれば、</p>
-                <p className="mb-3">村内の生活も豊かになるのではないだろうか。</p>
+            <div className="text-katurao-black font-body font-bold md:font-light text-sm md:text-2xl mb-28">
+                <p className="mb-3">葛尾村にはコンビニが中央部に一軒しかない。<br />
+                    買い物には損害まで車を走らせる必要がある。<br />
+                    一方、村内では体験型牧場など、新たなサービスも生まれつつある。<br />
+                    日々の買い物・新たな体験などの消費活動がもっと活発になれば、<br />
+                    村内の生活も豊かになるのではないだろうか。</p>
             </div>
         </div>
     )
