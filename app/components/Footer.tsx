674252cf11d28de6af06ee27b18ff0d5833a69e8
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 10:37:37 +0900

    mobile added footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <div className="bg-katurao-green w-full px-[10%] md:px-[15%] py-32">
-            <div className="text-white font-title text-2xl font-bold">
+        <div className="bg-katurao-green w-full px-[10%] md:px-[15%] py-16 md:py-32">
+            <div className="text-white font-title text-light md:text-2xl font-bold">
                 <p>かつらおハッカソン実行委員会</p>
                 <p>katuraohackathon2023@gmail.com</p>
             </div>
