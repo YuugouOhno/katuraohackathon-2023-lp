9ca465e858d2cdeffc978c71349442b2d8901978
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 10:36:29 +0900

    mobile added about page

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,22 +1,22 @@
 export default function About() {
     return (
         <div className="bg-katurao-creem w-full px-[10%] md:px-[15%]">
-            <p className="text-katurao-green text-title text-7xl font-bold my-24">About</p>
+            <p className="text-katurao-green text-title text-7xl font-bold my-16 md:my-24">About</p>
             <div className="px-[5%] text-xl">
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">テーマ</p>
-                    <p className="font-body">「葛尾村の消費活動をもっとワクワクするものへ。」</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">テーマ</p>
+                    <p className="font-body font-bold text-center md:text-left">「つながり」</p>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">期待すること</p>
-                    <p className="mb-3">・消費活動を通じ人のつながりを醸成</p>
-                    <p>・新しい生活様式の提案 など</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">期待すること</p>
+                    <p className="mb-3 font-bold md:font-light text-sm md:text-xl">・消費活動を通じ人のつながりを醸成<br />
+                        ・新しい生活様式の提案 など</p>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">期間</p>
-                    <p>2023年9月4日-9月16日</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 mb-2">期間</p>
+                    <p className="font-bold md:font-light text-sm md:text-xl">2023年9月4日-9月16日</p>
                 </div>
-                <div className="mb-20">
+                <div className="mb-10 md:mb-20">
                     <div className="flex h-40 flex items-center">
                         <div className="bg-katurao-red text-white font-bold font-title rounded-full w-40 h-40 text-center flex items-center justify-center">
                             <div>
@@ -76,45 +76,59 @@ export default function About() {
                         </div>
                     </div>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">会場</p>
-                    <p>葛尾村宿泊施設せせらぎ荘</p>
-                    <div>写真とか地図とか</div>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">会場</p>
+                    <p className="font-bold md:font-light text-sm md:text-xl">葛尾村宿泊施設せせらぎ荘</p>
+                    <div className="h-40 w-full bg-white p-10 my-10">写真とか地図とか</div>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">定員</p>
-                    <p>15名（5名×3チーム）</p>
-                    <p>※応募者多数の場合は選考の可能性があります。</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">定員</p>
+                    <p className="font-bold md:font-light text-sm md:text-xl">15名（5名×3チーム）<br />
+                        ※応募者多数の場合は選考の可能性があります。</p>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">参加対象</p>
-                    <p>高専生(4年生以上)・大学生・大学院生。</p>
-                    <p>・原則として1チーム５名の参加となりますが、5名に満たないチームでも応募可能です。</p>
-                    <p>・複数の高専、大学の学生から構成されるチームでも応募できます。</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">参加対象</p>
+                    <p className="font-bold md:font-light text-sm md:text-xl mb-2">
+                        高専生(4年生以上)・大学生・大学院生。
+                    </p>
+                    <p className="font-bold md:font-light text-sm md:text-xl md:hidden block">
+                        ・原則、1チーム５名の参加となりますが、<br />
+                        5名に満たないチームでも応募可能です。<br />
+                        ・複数の高専、大学の学生から構成される<br />
+                        チームでも応募できます。
+                    </p>
+                    <p className="font-bold md:font-light text-sm md:text-xl md:block hidden">
+                        ・原則、1チーム５名の参加となりますが、
+                        5名に満たないチームでも応募可能です。<br />
+                        ・複数の高専、大学の学生から構成される
+                        チームでも応募できます。
+                    </p>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">参加費</p>
-                    <p>なし。</p>
-                    <p>※交通費・食費は参加者負担となります。</p>
-                    <p>宿泊費は実行委員会からの全額負担となります。</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">参加費</p>
+                    <p className="font-bold md:font-light text-sm md:text-xl">
+                        なし。<br />
+                        ※交通費・食費は参加者負担となります。<br />
+                        宿泊費は実行委員会からの全額負担となります。</p>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2">申込締切</p>
-                    <p>7月28日まで。</p>
-                    <p>選考結果は8月4日頃代表者へご連絡します。</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title md:border-b md:border-katurao-black pb-2 md:mb-2">申込締切</p>
+                    <p className="font-bold md:font-light text-sm md:text-xl">
+                        7月28日まで。<br />
+                        選考結果は8月4日頃代表者へご連絡します。</p>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">表彰</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">表彰</p>
                     <div>WED賞</div>
                     <div>スカラ賞</div>
                 </div>
-                <div className="text-katurao-black mb-20">
-                    <p className="font-bold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">協賛</p>
+                <div className="text-katurao-black mb-10 md:mb-20">
+                    <p className="font-extrabold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">協賛</p>
                     <div>ロゴやらなんやら</div>
                     <div>ロゴやらなんやら</div>
                     <div>ロゴやらなんやら</div>
                 </div>
-                <div className="flex justify-center mb-20">
+                <div className="flex justify-center mb-10 md:mb-20">
                     <div className="bg-katurao-green text-center inline-block rounded-xl py-2 px-8">
                         <p className="text-white font-bold font-title text-2xl">
                             ご応募はこちら
