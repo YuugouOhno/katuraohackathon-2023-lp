d3c7ef5410ccbfbaea66c2bd0c6ad4e373f312df
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-10 09:50:13 +0900

    タイトルの設定

--- app/components/Top.tsx
+++ app/components/Top.tsx
@@ -1,8 +1,10 @@
 export default function Top() {
     return (
         <div className="bg-katurao-creem w-full px-[15%] pt-32 pb-48 font-title font-black">
-            <p className="text-katurao-green text-8xl">かつらお</p>
-            <p className="text-katurao-green text-8xl">ハッカソン</p>
+            <p className="text-katurao-green text-8xl">
+                かつらお<br/>
+                ハッカソン
+                </p>
             <p className="text-katurao-black text-8xl">2023</p>
         </div>
     )
