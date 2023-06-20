577f5da3ab137019b5363edbde23caa7966308a7
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-20 21:41:59 +0900

    added message

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -3,11 +3,10 @@ export default function Message() {
         <div className="bg-katurao-yellow  w-full px-[10%] md:px-[15%] md:text-center items-center">
             <p className="text-white text-5xl md:text-9xl font-title font-black mt-14 mb-10 md:my-28">Message</p>
             <div className="font-title font-black text-2xl md:text-3xl">
-                <p className="text-katurao-black mb-1 md:mb-3">葛尾村の消費活動を</p>
-                <p className="text-katurao-black mb-12">もっとワクワクするものへ。</p>
+                <p className="text-katurao-black mb-12">先輩からのメッセージ</p>
             </div>
             <div className="text-katurao-black font-body font-bold md:font-light text-sm md:text-2xl mb-28">
-                <p className="mb-3">余田さんのメッセージが入る</p>
+                <p className="mb-3">ものづくりを通して、これまでにないものを作る。かつらおハッカソン2023は、はそんなハッカソンです。皆さまには福島県葛尾村(かつらおむら)・東京都内会場の2拠点を舞台として、2週間の期間で「つながり」というテーマでの開発を取り組んでいただきます。テーマにマッチした内容であれば、Webサービスでもモバイルアプリケーションなど、形式は不問。皆さまのチャレンジをお待ちしています！</p>
             </div>
         </div>
     )
