775743c3112f5a658675e5f2000e9793e836e108
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-20 21:59:42 +0900

    added about

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -25,7 +25,7 @@ export default function About() {
                         </div>
                         <div className="text-katurao-black ml-8 md:ml-16 font-bold text-sm md:text-xl">
                             <p className="font-bold font-title mb-3">Step01</p>
-                            <p className="font-bold md:font-light">キックオフ＠都内</p>
+                            <p className="font-bold md:font-light">キックオフイベント<span className="border-b-2 border-katurao-black">@都内会場</span></p>
                         </div>
                     </div>
 
@@ -39,7 +39,7 @@ export default function About() {
                         </div>
                         <div className="text-katurao-black ml-8 md:ml-16 font-bold text-sm md:text-xl">
                             <p className="font-bold font-title mb-3">Step02</p>
-                            <p className="font-bold md:font-light">オンライン講座＠都内</p>
+                            <p className="font-bold md:font-light">中間FB<span className="border-b-2 border-katurao-black">@都内会場</span></p>
                         </div>
                     </div>
 
@@ -53,7 +53,7 @@ export default function About() {
                         </div>
                         <div className="text-katurao-black ml-8 md:ml-16 font-bold text-sm md:text-xl">
                             <p className="font-bold font-title mb-3">Step03</p>
-                            <p className="font-bold md:font-light">開発合宿＠かつらお村</p>
+                            <p className="font-bold md:font-light">開発合宿<span className="border-b-2 border-katurao-black">＠かつらお村</span></p>
                         </div>
                     </div>
 
@@ -68,7 +68,7 @@ export default function About() {
                         </div>
                         <div className="text-katurao-black ml-8 md:ml-16 font-bold text-sm md:text-xl">
                             <p className="font-bold font-title mb-3">Step04</p>
-                            <p className="font-bold md:font-light">最終発表会＠かつらお村</p>
+                            <p className="font-bold md:font-light">最終発表会<span className="border-b-2 border-katurao-black">＠かつらお村</span></p>
                         </div>
                     </div>
                 </div>
@@ -102,8 +102,10 @@ export default function About() {
                     </p>
                     <p className="font-bold md:font-light text-sm md:text-xl">
                         宿泊費全額補助あり<br />
-                        交通費最大8800円の補助あり<br />
-                        例:東京-郡山間で学割利用で往復15040円のうち、8800円の補助が可能<br />
+                        居住地によって交通費補助あり<br />
+                        例東京都→最大8000円　千葉県→9000円　神奈川県→10000円など<br />
+                        東京-郡山間で新幹線学割利用で往復15040円のうち、8000円の補助が可能
+
                     </p>
                 </div>
                 <div className="text-katurao-black mb-10 md:mb-20">
@@ -114,14 +116,47 @@ export default function About() {
                 </div>
                 <div className="text-katurao-black mb-10 md:mb-20">
                     <p className="font-extrabold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">表彰</p>
-                    <div>WED賞</div>
-                    <div>スカラ賞</div>
+                    <div className="mb-6">
+
+
+                        <p className="text-4xl font-bold font-title mb-2">WED賞</p>
+                        <p className="text-2xl font-bold font-title">対象</p>
+                        <p className="text-light">最もさわって楽しいものを作ったチーム</p>
+                        <p className="text-2xl font-bold font-title">賞金</p>
+                        <p className="text-light">シークレット（15万円相当のプレゼントを贈呈）</p>
+                        <p className="text-2xl font-bold font-title">副賞</p>
+                        <p className="text-light">CEOとのお食事会特別招待　and more</p>
+                    </div>
+
+                    <div className="">
+                        <p className="text-4xl font-bold font-title mb-2">スカラ賞</p>
+                        <p className="text-2xl font-bold font-title">対象</p>
+                        <p className="text-light">最もチャレンジをしたチーム</p>
+                        <p className="text-2xl font-bold font-title">賞金</p>
+                        <p className="text-light">３万円×人数分</p>
+                        <p className="text-2xl font-bold font-title">副賞</p>
+                        <p className="text-light">お食事会への特別招待</p>
+                    </div>
                 </div>
                 <div className="text-katurao-black mb-10 md:mb-20">
                     <p className="font-extrabold font-title border-b border-katurao-black pb-2 mb-2 text-5xl">協賛</p>
-                    <div>ロゴ 参加者への200字程度での応援メッセージ</div>
-                    <div>ロゴ 参加者への200字程度での応援メッセージ</div>
-                    <div>ロゴ 参加者への200字程度での応援メッセージ</div>
+                    <div className="mb-6">
+                        <img src="./wed.png" alt="WED株式会社様" />
+                        <p>WED は「さわって楽しいものを作る。」を宣言し、累計 600 万 DL を誇るお金がもらえるお買い物アプリ「ONE」をはじめ、ワクワクする魔法体験を生み出しています。
+                            15 歳で起業した CEO 山内を筆頭に、およそ 60 名のスペシャリストたちと社会現象を実装してきました。そして次なる現象を生み出すために、山内率いる WED が次世代を担う皆さんの個性と才能を発揮するインキュベーションの場を提供します。
+                            私たちと一緒に、つくって、さわって楽しいものづくりをしましょう！
+                        </p>
+                    </div>
+                    <div>
+                        <img src="./scp.png" alt="株式会社スカラパートナーズ様" />
+                        <p>震災を乗り越え、今年で村制100周年を迎えた葛尾村。
+                            ITを駆使した新時代へと歩みを進めている この地でハッカソンが開催されます。
+                            人と人、技術と発想、そして現在と未来。
+                            これらをつなぎ合わせ、皆さんが「エンジニア」にとどまらず、広い視野を持ち新たな価値を創造する「イノベーター」へと成長していくことを、私たちは全力でサポートします。
+                            新たな「つながり」を創り出すこの旅を通じて、葛尾村の未来を一緒に創り上げましょう。
+                        </p>
+                    </div>
+
                 </div>
                 <div className="flex justify-center mb-10 md:mb-20">
                     <div className="bg-katurao-green text-center inline-block rounded-xl py-2 px-8">
