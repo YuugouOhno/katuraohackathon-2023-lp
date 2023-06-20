03ccb59cdbf4a116dfc2882d41b5c43905db31ff
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-20 22:10:10 +0900

    サムネ？

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'かつらおハッカソン',
   description: 'かつらおハッカソンの公式ページです。',
+  image: "https://res.cloudinary.com/dzxep0pa9/image/upload/v1687266566/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88_2023-06-20_22.08.14_i3euja.png"
 }
 
 export default function RootLayout({
