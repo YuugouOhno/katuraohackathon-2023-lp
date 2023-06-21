acb2dd672ad3f3c7feb056a645e52d4635342dfc
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-21 10:35:44 +0900

    改行

--- app/components/Top.tsx
+++ app/components/Top.tsx
@@ -8,7 +8,9 @@ export default function Top() {
                     ハッカソン
                 </p>
                 <p className="text-katurao-black text-5xl md:text-8xl">2023</p>
-                <p className="text-katurao-black">ものづくりを通して、これまでにないものを作る。かつらおハッカソン2023はそんなハッカソンです。皆さまには福島県葛尾村(かつらおむら)・東京都内会場の2拠点を舞台として、2週間の期間で「つながり」というテーマでの開発を取り組んでいただきます。テーマにマッチした内容であれば、Webサービスでもモバイルアプリケーションなど、形式は不問。皆さまのチャレンジをお待ちしています！</p>
+                <p className="text-katurao-black">ものづくりを通して、これまでにないものを作る。かつらおハッカソン2023はそんなハッカソンです。<br />
+                皆さまには福島県葛尾村(かつらおむら)・東京都内会場の2拠点を舞台として、2週間の期間で「つながり」というテーマでの開発を取り組んでいただきます。<br />
+                テーマにマッチした内容であれば、Webサービスでもモバイルアプリケーションなど、形式は不問。皆さまのチャレンジをお待ちしています！</p>
             </div>
             <div className="px-20 md:pt-72 pt-32 absolute inset-0 w-full h-full object-cover flex justify-center items-center">
 
