5c11de813f618c4e639abfd05bed3d8fbadac653
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-21 14:07:44 +0900

    change mail

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <div className="bg-katurao-green w-full px-[10%] md:px-[15%] py-16 md:py-32">
             <div className="text-white font-title text-light md:text-2xl font-bold">
                 <p>かつらおハッカソン実行委員会</p>
-                <p>katuraohackathon2023@gmail.com</p>
+                <p>katsuraohackathon2023@gmail.com</p>
             </div>
         </div>
     )
